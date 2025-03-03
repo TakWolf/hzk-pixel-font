e5e2055e769a2613a51a57d21204c19423e21a59
TakWolf <takwolf@foxmail.com>
2025-03-03 20:18:56 +0800

    优化构建逻辑

--- tools/services/publish_service.py
+++ tools/services/publish_service.py
@@ -22,27 +22,24 @@ def make_release_zips():
 
 
 def update_docs():
-    for file_dir, _, file_names in path_define.outputs_dir.walk():
-        for file_name in file_names:
-            if re.match(r'preview-.*px\.png', file_name) is None:
-                continue
-            path_from = file_dir.joinpath(file_name)
-            path_to = path_define.docs_dir.joinpath(path_from.relative_to(path_define.outputs_dir))
-            path_to.parent.mkdir(parents=True, exist_ok=True)
-            shutil.copyfile(path_from, path_to)
-            logger.info("Copy file: '{}' -> '{}'", path_from, path_to)
+    path_define.docs_dir.mkdir(parents=True, exist_ok=True)
+
+    for path_from in path_define.outputs_dir.iterdir():
+        if re.match(r'preview-.*px\.png', path_from.name) is None:
+            continue
+        path_to = path_define.docs_dir.joinpath(path_from.name)
+        shutil.copyfile(path_from, path_to)
+        logger.info("Copy file: '{}' -> '{}'", path_from, path_to)
 
 
 def update_www():
     if path_define.www_fonts_dir.exists():
         shutil.rmtree(path_define.www_fonts_dir)
+    path_define.www_fonts_dir.mkdir(parents=True)
 
-    for file_dir, _, file_names in path_define.outputs_dir.walk():
-        for file_name in file_names:
-            if re.match(r'.*\.woff2', file_name) is None:
-                continue
-            path_from = file_dir.joinpath(file_name)
-            path_to = path_define.www_fonts_dir.joinpath(path_from.relative_to(path_define.outputs_dir))
-            path_to.parent.mkdir(parents=True, exist_ok=True)
-            shutil.copyfile(path_from, path_to)
-            logger.info("Copy file: '{}' -> '{}'", path_from, path_to)
+    for path_from in path_define.outputs_dir.iterdir():
+        if re.match(r'.*\.woff2', path_from.name) is None:
+            continue
+        path_to = path_define.www_fonts_dir.joinpath(path_from.name)
+        shutil.copyfile(path_from, path_to)
+        logger.info("Copy file: '{}' -> '{}'", path_from, path_to)
