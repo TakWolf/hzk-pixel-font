90d2f7388fbccd3632ade375bbe379075339a70e
TakWolf <takwolf@foxmail.com>
2025-03-13 16:19:06 +0800

    修复调用

--- tools/services/font_service.py
+++ tools/services/font_service.py
@@ -45,13 +45,13 @@ def _create_builder(font_config: FontConfig, character_mapping: dict[int, str], 
     builder.character_mapping.update(character_mapping)
 
     for glyph_file in glyph_sequence:
-        horizontal_origin_y = (font_config.ascent + font_config.descent - glyph_file.height) // 2
-        vertical_origin_x = -math.ceil(glyph_file.width / 2)
+        horizontal_offset_y = (font_config.ascent + font_config.descent - glyph_file.height) // 2
+        vertical_offset_x = -math.ceil(glyph_file.width / 2)
         builder.glyphs.append(Glyph(
             name=glyph_file.glyph_name,
-            horizontal_origin=(0, horizontal_origin_y),
+            horizontal_offset=(0, horizontal_offset_y),
             advance_width=glyph_file.width,
-            vertical_origin=(vertical_origin_x, 0),
+            vertical_offset=(vertical_offset_x, 0),
             advance_height=font_config.font_size,
             bitmap=glyph_file.bitmap.data,
         ))
